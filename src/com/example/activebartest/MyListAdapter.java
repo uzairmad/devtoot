7380000f8aab88cd1dbd307ff2ca33e73fe6eec5
uzairmad <uzairm@gmail.com>
2014-07-24 11:47:39 -0400

    pushed mylistadapter

---
+++ src/com/example/activebartest/MyListAdapter.java
@@ -0,0 +1,42 @@
+package com.example.activebartest;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class MyListAdapter extends ArrayAdapter<String> {
+
+	Context context;
+
+	public MyListAdapter(Context context, ArrayList<String> options) {
+		super(context, R.layout.option_list_item, options);
+
+		this.context = context;
+	}
+	
+	 @Override
+	    public View getView(int position, View convertView, ViewGroup parent) {
+		        View view = super.getView(position, convertView, parent);
+		    	TextView tv =(TextView) view.findViewById(R.id.rowTextView);
+	        if (position == 0) {
+	            // set your color
+	        	//make description item appear different due to more text	        
+	        	tv.setBackgroundColor(Color.parseColor("#3059AC")); //cobaltish
+	    		tv.setTextColor(Color.parseColor("#D8D8DB")); //light grey
+	    		tv.setTextSize(TypedValue.COMPLEX_UNIT_SP,16);
+	        	
+	        }else{
+    			tv.setTextSize(TypedValue.COMPLEX_UNIT_SP,24);
+    		
+    		}
+
+	        return view;
+	    }
+	
+}
